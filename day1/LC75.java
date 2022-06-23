852f3fc49d05b9389d475b8d823dbe86ace41c21
SHhivamSharma <shivamsharma9868789@gmail.com>
2022-06-24 02:32:50 +0530

    1.6

---
+++ day1/LC75.java
@@ -0,0 +1,26 @@
+//75. Sort Colors
+class Solution {
+    public void sortColors(int[] nums) {
+        int mid = 0;
+        int low = 0 ;
+        int high = nums.length -1;
+        while( mid <= high ){
+            if(nums[mid] == 0){
+                swap (nums , mid , low);
+                low++;
+                mid++;
+            } else if( nums [mid] == 1){
+                mid++;
+            } else {
+                      swap (nums , mid , high);
+                high--;
+
+            }
+        }
+    }
+     public static void swap ( int[] nums ,int a ,int  b ){
+         int temp = nums[a];
+         nums[a] = nums[b];
+         nums[b] = temp ;
+     }
+}
